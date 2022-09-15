e2a767642b2cc6b9f97fdbf7eb5db2f35f822e05
Armin Eyvazi <genius2001armin@gmail.com>
2022-09-16 00:47:44 +0430

    Add loops

--- src/lectures/demo/loops/loops.go
+++ src/lectures/demo/loops/loops.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-
+	sum:=0
+	fmt.Println("Sum is",sum)
+	//print 1,3,5,7,9
+	for i:=1;i<10;i++{
+		if i%2==0{
+			continue
+		}
+		fmt.Println(i)
+	}
+	for i:=0;i<10;i++{
+		sum+=i
+		fmt.Println("sum is :",sum)
+	}
+	
+	for sum>10{
+		sum-=5
+		fmt.Println("Decrement .sum is",sum)
+	}
 }
