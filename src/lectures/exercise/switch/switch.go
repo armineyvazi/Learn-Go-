1c410e34735a29c15fdd13a938e0b7727975cc99
Armin Eyvazi <genius2001armin@gmail.com>
2022-09-15 21:13:58 +0430

    Do exercise switch

--- src/lectures/exercise/switch/switch.go
+++ src/lectures/exercise/switch/switch.go
@@ -3,15 +3,29 @@
 //
 //--Requirements:
 //* Use a `switch` statement to print the following:
-//  - "newborn" when age is 0
-//  - "toddler" when age is 1, 2, or 3
-//  - "child" when age is 4 through 12
-//  - "teenager" when age is 13 through 17
-//  - "adult" when age is 18+
+
 
 package main
 
 import "fmt"
 
 func main() {
+	
+	switch age:=0;{
+		case age==0:
+			//- "newborn" when age is 0
+			fmt.Println("New born")	
+		case age>1 && age <=3:
+			//  - "toddler" when age is 1, 2, or 3
+			fmt.Println("tollder")
+		case age<13:
+			//  - "child" when age is 4 through 12
+			fmt.Println("Child")
+		case age<18:
+			//  - "teenager" when age is 13 through 17
+			fmt.Println("teenager")
+		default:
+			//  - "adult" when age is 18
+			fmt.Println("adult")
+	}
 }
