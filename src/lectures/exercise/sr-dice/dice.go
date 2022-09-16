0544dabab1ce74d96684579dfc5e746a9ecd1bbe
Armin Eyvazi <genius2001armin@gmail.com>
2022-09-17 00:21:17 +0430

    Add Mini Project dice

--- src/lectures/exercise/sr-dice/dice.go
+++ src/lectures/exercise/sr-dice/dice.go
@@ -5,10 +5,6 @@
 //--Requirements:
 //* Print the sum of the dice roll
 //* Print additional information in these cirsumstances:
-//  - "Snake eyes": when the total roll is 2, and total dice is 2
-//  - "Lucky 7": when the total roll is 7
-//  - "Even": when the total roll is even
-//  - "Odd": when the total roll is odd
 //* The program must handle any number of dice, rolls, and sides
 //
 //--Notes:
@@ -17,6 +13,45 @@
 package main
 
 import "fmt"
+import "time"
+import "math/rand"
+
+func roll(sides int)int{
+	return rand.Intn(sides)+1
+}
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	dice,sides:=2,12
+	rolls:=1
+
+	for r:=1;r<=rolls;r++{
+		sum:=0
+		
+		for d:=1;d<=dice;d++{
+			rolled:=roll(sides)
+			sum+=rolled
+			fmt.Println("Roll #",r,"Die#",d,":",rolled)
+		}
+		fmt.Println("Total rolled:",sum)
+
+		switch sum := sum;{
+			case sum==2 && dice==2:{
+			//  - "Snake eyes": when the total roll is 2, and total dice is 2
+				fmt.Println("Snake Eyes")
+			}
+			case sum==7:{
+			//  - "Lucky 7": when the total roll is 7
+				fmt.Println("Lucky")
+			}
+			case sum%2==0:{
+			//  - "Even": when the total roll is even
+				fmt.Println("Even")
+			}
+			case sum%2!=0:{
+			//  - "Odd": when the total roll is odd
+				fmt.Println("Odd")
+			}
+		}
+	}
 }
