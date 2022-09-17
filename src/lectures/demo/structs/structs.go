d518894ddbf1116af9377318fe559d04b48c1f6d
Armin Eyvazi <genius2001armin@gmail.com>
2022-09-17 11:56:44 +0430

    Add Struct

--- src/lectures/demo/structs/structs.go
+++ src/lectures/demo/structs/structs.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
-func main() {
+type Passenger struct{
+	Name		 string
+	TicketNumber int
+	Boarded 	 bool
+}
+type Bus struct{
+	FrontSeat Passenger
+}
 
+func main() {
+	
+	casey:=Passenger{"casey",222,false}
+	fmt.Println(casey)
+
+	var(
+		bill 	= Passenger{Name: "bill",TicketNumber: 2222}
+		ella 	= Passenger{Name: "ella",TicketNumber: 22  }
+	)
+	fmt.Println(ella,bill)
+
+	var heidi Passenger
+	heidi.Name		   = "heidi"
+	heidi.TicketNumber = 222222
+	heidi.Boarded	   = true
+	fmt.Println(heidi)
+
+	casey.Boarded = true
+	bill.Boarded  = true
+
+	if bill.Boarded{
+		fmt.Println("Bill has boarded the bus")
+	}
+	if casey.Boarded{
+		fmt.Println("Casey has boarded the bus")
+	}
+
+	heidi.Boarded = true
+	bus:=Bus{heidi}
+	fmt.Println(bus)
+	fmt.Println(bus.FrontSeat.Name,"is in the front seat")	
 }
