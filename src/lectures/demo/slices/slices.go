cf4ddca60c715fe14e977f726c03f999c5c685b8
Armin Eyvazi <genius2001armin@gmail.com>
2022-10-01 10:13:58 +0330

    Add Slice

--- src/lectures/demo/slices/slices.go
+++ src/lectures/demo/slices/slices.go
@@ -1,7 +1,32 @@
 package main
 
 import "fmt"
+func printSlice(title string,slice []string){
+	fmt.Println()
+	fmt.Println("----",title,"-----")
+	for i:=0;i<len(slice);i++{
+		element:=slice[i]
+		fmt.Println(element)
+	}
+}
 
 func main() {
+
+	route:=[]string{"Georcy","Department Story","Salon"}
+    
+	printSlice("armin",route)
+
+	route = append(route, "Home")
+	printSlice("Route 2",route)
+
+	fmt.Println()
+
+	fmt.Println(route[0],"visited")
+	fmt.Println(route[1],"visited")
+	fmt.Println(route[2],"visited")
+
+	route = route[2:]
+
+	printSlice("Remaining location",route)
 
 }
