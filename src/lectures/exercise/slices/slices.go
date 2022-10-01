9fa70e9dcae4828dc3f0bcda04a7d758d6336fd2
Armin Eyvazi <genius2001armin@gmail.com>
2022-10-01 20:14:52 +0330

    Add exercise slice

--- src/lectures/exercise/slices/slices.go
+++ src/lectures/exercise/slices/slices.go
@@ -2,12 +2,6 @@
 //  Create a program to manage parts on an assembly line.
 //
 //--Requirements:
-//* Using a slice, create an assembly line that contains type Part
-//* Create a function to print out the contents of the assembly line
-//* Perform the following:
-//  - Create an assembly line having any three parts
-//  - Add two new parts to the line
-//  - Slice the assembly line so it contains only the two new parts
 //  - Print out the contents of the assembly line at each step
 //--Notes:
 //* Your program output should list 3 parts, then 5 parts, then 2 parts
@@ -17,6 +11,30 @@ package main
 import "fmt"
 
 type Part string
-
+//* Create a function to print out the contents of the assembly line
+func showLine(line []Part) {
+	for i:=0;i<len(line); i++{
+		part:=line[i]
+		fmt.Println(part)
+	}
+}
 func main() {
+	//* Using a slice, create an assembly line that contains type Part
+	assemblyLine:=make([]Part, 3)
+	//- Create an assembly line having any three parts
+	assemblyLine[0]="pipe"
+	assemblyLine[1]="Bolt"
+	assemblyLine[2]="Sheet"
+
+	fmt.Println("3 Parts")
+	showLine(assemblyLine)
+	//  - Add two new parts to the line
+	assemblyLine =  append(assemblyLine, "washer","wheel")
+	fmt.Println("\nAdded two parts:")
+	showLine(assemblyLine)
+	
+	//  - Slice the assembly line so it contains only the two new parts
+	assemblyLine=assemblyLine[2:]
+	fmt.Println("\nSlice:")
+	showLine(assemblyLine)
 }
