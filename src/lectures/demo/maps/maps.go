d5a23f7443eb3e567d421f417b86df47b18abb77
Armin Eyvazi <genius2001armin@gmail.com>
2022-10-03 21:37:26 +0330

    add map

--- src/lectures/demo/maps/maps.go
+++ src/lectures/demo/maps/maps.go
@@ -3,4 +3,32 @@ package main
 import "fmt"
 
 func main() {
+	
+	shoppingList:=make(map[string]int)
+	shoppingList["eggs"]	=11
+	shoppingList["milk"]	=1
+	shoppingList["bread"]	+=1
+
+
+	shoppingList["eggs"]	+=1
+	fmt.Println(shoppingList)
+
+	delete(shoppingList,"milk")
+	fmt.Println("Milk deleted","new List:",shoppingList)
+
+	fmt.Println("need",shoppingList["eggs"],"eggs")
+
+	ceral,found:=shoppingList["ceral"]
+	if !found{
+		fmt.Println("nop")
+	}else{
+		fmt.Println("yup",ceral,"boxes")
+	}
+	
+	totalItem:=0
+	for _,amount:=range shoppingList{
+		totalItem+=amount
+	}
+	fmt.Println("There are:",totalItem,"on the shopping list")
+
 }
