4e9577a43b3af0017cb8b53e3faca0731316ba73
Armin Eyvazi <genius2001armin@gmail.com>
2022-10-03 21:39:14 +0330

    Add ranges

--- src/lectures/demo/ranges/ranges.go
+++ src/lectures/demo/ranges/ranges.go
@@ -3,4 +3,23 @@ package main
 import "fmt"
 
 func main() {
+
+	slice:=[]string{"hello","world","!"}
+
+	for i,element:=range slice {
+		fmt.Println(i,element,":")
+		for _,ch:=range element {
+			fmt.Printf("%q\n",ch)
+		}
+	}
+	
+	for i:=0;i<len(slice);i++{
+		fmt.Println("slice",slice[i],":")
+		char:=slice[i]
+		for i:=0;i<len(char);i++{
+			ch:=char[i]
+			fmt.Printf("%q\n",ch)
+		}
+	}
+
 }
