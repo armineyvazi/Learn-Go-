9a21aed4bd477b4ec1fea3308c95ca1bb11b0851
Armin Eyvazi <genius2001armin@gmail.com>
2022-10-03 22:48:49 +0330

    add maps

--- src/lectures/exercise/maps/maps.go
+++ src/lectures/exercise/maps/maps.go
@@ -2,22 +2,19 @@
 //  Write a program to display server status.
 //
 //--Requirements:
-//* Create a function to print server status, including:
-//  - Number of servers
+
+
 //  - Number of servers for each status (Online, Offline, Maintenance, Retired)
 //* Store the existing slice of servers in a map
 //* Default all of the servers to `Online`
 //* Perform the following status changes and display server info:
-//  - display server info
-//  - change `darkstar` to `Retired`
-//  - change `aiur` to `Offline`
-//  - display server info
-//  - change all servers to `Maintenance`
-//  - display server info
+
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	Online      = 0
@@ -25,7 +22,52 @@ const (
 	Maintenance = 2
 	Retired     = 3
 )
+//* Create a function to print server status, including:
+func printStatusServer(servers map[string]int){
+	//  - Number of servers
+	fmt.Println("\nThere are ",len(servers),"servers")
+	stats:=make(map[int]int)
+	for _,status:=range servers{
+		switch status {
+		case Online:
+			stats[Online]+=1
+		case Offline:
+			stats[Offline]+=1
+		case Maintenance:
+			stats[Maintenance]+=1
+		case Retired:
+			stats[Retired]+=1
+		default:
+			panic("unhandeled server status")
+		}
+	}
+	fmt.Println(stats[Online],"server online server")
+	fmt.Println(stats[Offline],"server online Offline")
+	fmt.Println(stats[Maintenance],"server online Maintenance")
+	fmt.Println(stats[Retired],"server online Retired")
+ 
+}
 
 func main() {
+
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+	serverStatus:=make(map[string]int)
+	for _,server := range servers{
+		serverStatus[server]=Online
+	}
+
+	//  - display server info
+	printStatusServer(serverStatus)
+	//  - change `darkstar` to `Retired`
+	serverStatus["darkstar"]=Retired
+	//  - change `aiur` to `Offline`
+	serverStatus["aiur"]=Offline
+	//  - display server info
+	printStatusServer(serverStatus)
+	//  - change all servers to `Maintenance`
+	for key,_:=range serverStatus{
+		serverStatus[key]=Maintenance
+	}
+	//  - display server info
+	printStatusServer(serverStatus)
 }
