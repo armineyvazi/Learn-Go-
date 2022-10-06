29108530a38da979e11c3f41a15629b2d4e98efd
armin <genius2001armin@gmail.com>
2022-10-06 14:25:28 +0330

    Add pointer

--- src/lectures/demo/pointers/pointers.go
+++ src/lectures/demo/pointers/pointers.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+type Counter struct {
+	hits int
+}
+
+func increment(counter *Counter) {
+	counter.hits += 1
+	fmt.Println("Counter", counter)
+}
+func replace(old *string, new string, counter *Counter) {
+	*old = new
+	increment(counter)
+}
+
 func main() {
+
+	counter := Counter{}
+
+	hello := "Hello"
+	world := "world"
+	replace(&hello, "Hi", &counter)
+
+	fmt.Println(hello, world)
+
+	fmt.Println("---------------")
+
+	phrase:=[]string{hello,world}
+	fmt.Println(phrase)
+	
+	fmt.Println("---------------")
+
+	replace(&phrase[1],"Go",&counter)
+	fmt.Println(phrase)
 
 }
