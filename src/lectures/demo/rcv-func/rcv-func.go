7df308e6bbafc7ad3f5c7c9a5e51ca6d3d680f10
armin <genius2001armin@gmail.com>
2022-10-07 19:31:18 +0330

    rcv-func

--- src/lectures/demo/rcv-func/rcv-func.go
+++ src/lectures/demo/rcv-func/rcv-func.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
-func main() {
+type space struct {
+	occupied bool
+}
+type ParkingLot struct {
+	sapces []space
+}
 
+func occupySpace(lot *ParkingLot, spaceNumber int) {
+	lot.sapces[spaceNumber-1].occupied = true
+}
+//receiver function
+func (lot *ParkingLot)occupySpace(spaceNumber int){
+	lot.sapces[spaceNumber-1].occupied = true
+}
+func (lot *ParkingLot)vacateSpace(spaceNumber int){
+	lot.sapces[spaceNumber-1].occupied = false
+}
+
+func main() {
+	lot:=ParkingLot{sapces:make([]space, 4)}
+	fmt.Println("initial:",lot)
+	lot.occupySpace(1)
+	occupySpace(&lot,2)
+	fmt.Println("after occpuied",lot)
+	lot.vacateSpace(2)
+	fmt.Println("after vaated:",lot)
+
+	
+
+
+
+	
 }
