480dd2e485baf0a152ab342bb60eca1793c4a6c0
armin <genius2001armin@gmail.com>
2022-10-10 18:25:18 +0330

    iota exercise

--- src/lectures/exercise/iota/iota.go
+++ src/lectures/exercise/iota/iota.go
@@ -7,16 +7,45 @@
 //  on two operands
 //* Operations required:
 //  - Add, Subtract, Multiply, Divide
-//* The existing function calls in main() represent the API and cannot be changed
 //
 //--Notes:
 //* Your program is complete when it compiles and prints the correct results
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+const 
+	(
+		Add = iota
+		Subtract
+		Multiply
+		Divide
+	)
+type Operations int 
+
+func (op Operations)calculate(lhs,rhs int) int{
+
+	switch op{
+		case Add:
+			return lhs+rhs
+		case Subtract:
+			return lhs-rhs
+		case Multiply:
+			return lhs*rhs
+		case Divide:
+			return lhs/rhs
+	}
+	panic("Unhandle operation")
+}
 
 func main() {
+//* The existing function calls in main() represent the API and cannot be changed
+	add:=Operations(Add)
+	sub:=Operations(Subtract)
+	mul:=Operations(Multiply)
+	div:=Operations(Divide)
 	fmt.Println(add.calculate(2, 2)) // = 4
 
 	fmt.Println(sub.calculate(10, 3)) // = 7
@@ -24,4 +53,6 @@ func main() {
 	fmt.Println(mul.calculate(3, 3)) // = 9
 
 	fmt.Println(div.calculate(100, 2)) // = 50
+
+	
 }
