f2e303017e3773051141cf5f77d488ebac1d8641
armin <genius2001armin@gmail.com>
2022-10-10 18:39:48 +0330

    Add variadics

--- src/lectures/demo/variadics/variadics.go
+++ src/lectures/demo/variadics/variadics.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
+func sum(nums ...int) int {
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
+	return sum
+}
 
+func main() {
+	a := []int{1, 2, 3, 4}
+	b := []int{5, 6, 7, 8, 9, 10}
+	all := append(a, b...)
+	answer := sum(all...)
+	fmt.Println("all answer a and b", answer)
+	//or use
+	answer = sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	fmt.Println(answer)
 }
