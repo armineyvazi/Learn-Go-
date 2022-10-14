fc7fbc5e713794c4a55a23f8320b3fb0b37ce3b7
armin <genius2001armin@gmail.com>
2022-10-14 15:58:41 +0330

    testing

---
+++ src/lectures/demo/testing/queue_test.go
@@ -0,0 +1,41 @@
+package queue
+
+import "testing"
+
+func TestAddQueue(t *testing.T) {
+	q := New(3)
+	for i := 0; i < 3; i++ {
+		if len(q.items) != i {
+			t.Errorf("Incorrect queue element count:%v,want: %v", len(q.items), i)
+		}
+		if !q.Append(i) {
+			t.Errorf("failed to append item %v to queue", i)
+		}
+	}
+	if q.Append(4) {
+		t.Error("shoud not able to add to full queue")
+	}
+}
+func TesingNext(t *testing.T) {
+	q := New(3)
+	for i := 0; i < 3; i++ {
+		q.Append(i)
+	}
+
+	for i := 0; i < 3; i++ {
+		item, ok := q.Next()
+		if !ok {
+			t.Errorf("should be able to get item from queue")
+		}
+		if item != i {
+			t.Errorf("got item wrong order:%v,want: %v", item, i)
+		}
+	}
+
+	item, ok := q.Next()
+	//Queue is empty at this point
+	if ok {
+		t.Errorf("should not be any more items in queue,got:%v", item)
+	}
+
+}
