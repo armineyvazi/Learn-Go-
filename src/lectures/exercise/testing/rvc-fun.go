c1af7a85d6ba7b6829755ecfa7769c62466c75b6
armin <genius2001armin@gmail.com>
2022-10-15 14:40:19 +0330

    add exercise for testin

---
+++ src/lectures/exercise/testing/rvc-fun.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type Player struct {
+	name              string
+	health, maxHealth uint
+	energy, maxEnergy uint
+}
+
+func (player *Player) addhealth(amount uint) {
+	if player.health+amount < player.maxHealth {
+		player.health += amount
+		fmt.Println(player.name, "Add amount", amount, "health->", player.health)
+	} else {
+		fmt.Println("Your Health is max")
+	}
+
+}
+func (player *Player) addDamage(amount uint) {
+	if player.health-amount < player.health{
+		player.health -= amount
+		fmt.Println(player.name, "damage", amount, "damage->", player.health)
+	} else {
+		player.health = 0
+		fmt.Println(player.name, "dead:)")
+	}
+}
+func (player *Player) lessEnergy(amount uint) {
+	if player.energy-amount < player.energy {
+		player.energy -= amount
+		fmt.Print(player.name, "lessEnergy", amount, "essEnergy", player.energy)
+	} else {
+		player.energy = 0
+		fmt.Println("Not Enogh Energy")
+	}
+}
+func (player *Player) addEnergy(amount uint) {
+	if player.energy+amount < player.maxEnergy {
+		player.energy += amount
+		fmt.Println(player.name, "Add amount", amount, "energy->", player.energy)
+	} else {
+		fmt.Println("Your Energy is max")
+	}
+
+}
+func (player *Player) check(players Player) {
+	fmt.Println()
+	fmt.Println(players)
+}
