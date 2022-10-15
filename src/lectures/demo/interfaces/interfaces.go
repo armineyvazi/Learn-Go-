591beed8323d88e1209071f9d6e33516190fdb07
armin <genius2001armin@gmail.com>
2022-10-15 16:03:52 +0330

    add inteface

--- src/lectures/demo/interfaces/interfaces.go
+++ src/lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+type Preparer interface {
+	PrepareDish()
+}
+
+type Chiken string
+type Salad string
+
+func (c Chiken) PrepareDish() {
+	fmt.Println("cook chicken")
+}
+func (s Salad) PrepareDish() {
+	fmt.Println("chop salad")
+	fmt.Println("add dressing")
+}
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("Prepare dishes:")
+	for i := 0; i < len(dishes); i++ {
+		dish := dishes[i]
+		fmt.Printf("--Dish:%v", dish)
+		dish.PrepareDish()
+	}
+	fmt.Println()
+}
 func main() {
+	dishes := []Preparer{Chiken("Chicken wings"), Salad("Salad")}
+	prepareDishes(dishes)
 
 }
