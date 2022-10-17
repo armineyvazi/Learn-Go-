1edd31403704d592dd50e55f3dc0aefa3b97a376
armin <genius2001armin@gmail.com>
2022-10-18 00:01:02 +0330

    add exercise interface

--- src/lectures/exercise/interfaces/interfaces.go
+++ src/lectures/exercise/interfaces/interfaces.go
@@ -21,6 +21,61 @@ package main
 
 import "fmt"
 
+const (
+	SmallLift = iota
+	StandardLift
+	LargeLift
+)
+
+type Lift int
+
+type Liftpicker interface {
+	PickLift() Lift
+}
+type Motorcycles string
+type Car string
+type Trucks string
+
+func (m Motorcycles) String() string {
+	return fmt.Sprintf("Motorcycles:%v", string(m))
+}
+func (c Car) String() string {
+	return fmt.Sprintf("Car:%v", string(c))
+}
+func (t Trucks) String() string {
+	return fmt.Sprintf("Trucks:%v", string(t))
+}
+
+func (m Motorcycles) PickLift() Lift {
+	return SmallLift
+}
+func (c Car) PickLift() Lift {
+	return StandardLift
+}
+func (t Trucks) PickLift() Lift {
+	return LargeLift
+}
+
+func sendToLift(p Liftpicker) {
+	switch p.PickLift() {
+		case SmallLift:
+			fmt.Printf("send %v to small lift\n", p)
+			break
+		case StandardLift:
+			fmt.Printf("send %v to standard\n", p)
+			break
+		case LargeLift:
+			fmt.Printf("send %v to Large\n", p)
+			break
+	}
+}
+
 func main() {
+	car := Car("Sporty")
+	truck:=Trucks("MountainCrusher")
+	motor:=Motorcycles("Croozer")
+	sendToLift(car)
+	sendToLift(truck)
+	sendToLift(motor)
 
 }
