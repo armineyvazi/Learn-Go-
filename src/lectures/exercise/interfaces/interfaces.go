cb2316c737f28e236cb20ed897533e4e9fd95627
armin <genius2001armin@gmail.com>
2022-10-18 10:07:08 +0330

    add interface

--- src/lectures/exercise/interfaces/interfaces.go
+++ src/lectures/exercise/interfaces/interfaces.go
@@ -3,17 +3,6 @@
 //  to the correct vehicle lift, based on vehicle size.
 //
 //--Requirements:
-//* The shop has lifts for multiple vehicle sizes/types:
-//  - Motorcycles: small lifts
-//  - Cars: standard lifts
-//  - Trucks: large lifts
-//* Write a single function to handle all of the vehicles
-//  that the shop works on.
-//* Vehicles have a model name in addition to the vehicle type:
-//  - Example: "Truck" is the vehicle type, "Road Devourer" is a model name
-//* Direct at least 1 of each vehicle type to the correct
-//  lift, and print out the vehicle information.
-//
 //--Notes:
 //* Use any names for vehicle models
 
@@ -21,61 +10,69 @@ package main
 
 import "fmt"
 
+//  - Motorcycles: small lifts
+//  - Cars: standard lifts
+//  - Trucks: large lifts
 const (
-	SmallLift = iota
-	StandardLift
-	LargeLift
+	small = iota
+	standard
+	large
 )
-
 type Lift int
 
-type Liftpicker interface {
-	PickLift() Lift
+type park interface {
+	autopark() Lift
 }
 type Motorcycles string
-type Car string
+type Cars string
 type Trucks string
 
+//* Write a single function to handle all of the vehicles
 func (m Motorcycles) String() string {
 	return fmt.Sprintf("Motorcycles:%v", string(m))
 }
-func (c Car) String() string {
-	return fmt.Sprintf("Car:%v", string(c))
+func (c Cars) String() string {
+	return fmt.Sprintf("Cars:%v", string(c))
+
 }
 func (t Trucks) String() string {
 	return fmt.Sprintf("Trucks:%v", string(t))
 }
-
-func (m Motorcycles) PickLift() Lift {
-	return SmallLift
+//* The shop has lifts for multiple vehicle sizes/types:
+//  that the shop works on.
+//* Vehicles have a model name in addition to the vehicle type:
+//  - Example: "Truck" is the vehicle type, "Road Devourer" is a model name
+//* Direct at least 1 of each vehicle type to the correct
+//  lift, and print out the vehicle information.
+func (m Motorcycles) autopark() Lift {
+	return small
 }
-func (c Car) PickLift() Lift {
-	return StandardLift
+func (c Cars) autopark() Lift {
+	return standard
 }
-func (t Trucks) PickLift() Lift {
-	return LargeLift
+func (t Trucks) autopark() Lift {
+	return large
 }
-
-func sendToLift(p Liftpicker) {
-	switch p.PickLift() {
-		case SmallLift:
-			fmt.Printf("send %v to small lift\n", p)
+func autolift(l park) {
+	switch l.autopark() {
+		case small:
+			fmt.Printf("send %v to small lift\n",l)
 			break
-		case StandardLift:
-			fmt.Printf("send %v to standard\n", p)
+		case standard:
+			fmt.Printf("send %v to standard lift\n",l)
 			break
-		case LargeLift:
-			fmt.Printf("send %v to Large\n", p)
+		case large:
+			fmt.Printf("send %v to large lift\n",l)
 			break
 	}
 }
 
 func main() {
-	car := Car("Sporty")
-	truck:=Trucks("MountainCrusher")
-	motor:=Motorcycles("Croozer")
-	sendToLift(car)
-	sendToLift(truck)
-	sendToLift(motor)
+	motor 	:= Motorcycles("ktm")
+	cars 	:= Cars("sporto")
+	trucks 	:= Trucks("trucks")
 
+	autolift(motor)
+	autolift(cars)
+	autolift(trucks)
 }
