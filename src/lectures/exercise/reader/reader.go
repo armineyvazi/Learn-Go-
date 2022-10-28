db38010726c551cf3c1852de16f537e132bc5f70
armin <genius2001armin@gmail.com>
2022-10-28 21:44:07 +0330

    add reader exercise

--- src/lectures/exercise/reader/reader.go
+++ src/lectures/exercise/reader/reader.go
@@ -5,21 +5,53 @@
 //  have been ran, and how many lines of text was entered by the user.
 //
 //--Requirements:
-//* When the user enters either "hello" or "bye", the program
-//  should respond with a message after pressing the enter key.
-//* Whenever the user types a "Q" or "q", the program should exit.
-//* Upon program exit, some usage statistics should be printed
-//  ('Q' and 'q' do not count towards these statistics):
-//  - The number of non-blank lines entered
-//  - The number of commands entered
-//
+
 //--Notes
 //* Use any Reader implementation from the stdlib to implement the program
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	CmdHello = "hello"
+	CmdBye   = "bye"
+)
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	numLines := 0
+	numCommands := 0
+
+	for scanner.Scan() {
+		//* When the user enters either "hello" or "bye", the program
+		if strings.ToUpper(scanner.Text()) == "Q" {
+			break
+		} else {
+			text := strings.TrimSpace(scanner.Text())
+			//  should respond with a message after pressing the enter key.
+			//* Whenever the user types a "Q" or "q", the program should exit.
+			switch text {
+			case CmdHello:
+				numCommands += 1
+				fmt.Println("command respose:Hi")
+			case CmdBye:
+				numCommands += 1
+				fmt.Println("command response:Bye")
+			}
+			if text != "" {
+				numLines += 1
+			}
+
+		}
+	}
+
+	fmt.Printf("You entered %v lines\n", numLines)
+	fmt.Printf("You entered %v commands\n", numCommands)
 
 }
